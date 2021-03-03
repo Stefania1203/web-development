378ccf0e153ec1e7427ced796dfb2c1fda82fa67
Stefania1203 <79985326+Stefania1203@users.noreply.github.com>
2021-03-04 01:29:24 +0300

    Update RemoveExtraBlanks.php

--- lw3/RemoveExtraBlanks.php
+++ lw3/RemoveExtraBlanks.php
@@ -1,10 +1,10 @@
 <?php
-header("Content-Type: text/plain");
 $text = $_GET['document']; /* сохранили параметр text из GET в переменной */
 $word = explode(" ", $text);
 $space = '';
 foreach ($word as $w)   /* цикл происходит перебор значений в массиве word */
 {
+  header("Content-Type: text/plain");
   echo $space.$w;
   $space = ' ';
 }
