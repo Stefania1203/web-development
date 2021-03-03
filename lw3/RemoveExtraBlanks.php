c4c7e2082d9640f8bc4f8942645d81eb20e26b87
Stefania1203 <79985326+Stefania1203@users.noreply.github.com>
2021-03-04 02:02:25 +0300

    Update RemoveExtraBlanks.php

--- lw3/RemoveExtraBlanks.php
+++ lw3/RemoveExtraBlanks.php
@@ -3,12 +3,17 @@ header("Content-Type: text/plain");
 $text = $_GET['document']; /* сохранили параметр text из GET в переменной */
 if ($text === null) {
 	echo "Параметр name не найден";
-} 
-$word = explode(" ", $text);
-$space = '';
-foreach ($word as $w)   /* цикл происходит перебор значений в массиве word */
-{
-  echo $space.$w;
-  $space = ' ';
+}  else {
+	
+	echo "Параметр name без пробелов:".Remove($text);
 }
+function Remove(string $string) : string {
+         $word = explode(" ", $text);
+         $space = '';
+         foreach ($word as $w)   /* цикл происходит перебор значений в массиве word */
+         {
+            echo $space.$w;
+            $space = ' ';
+         }
+ }	
 ?>
