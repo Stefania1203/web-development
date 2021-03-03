eb23159a783059c60a90f8691530258e55838692
Stefania1203 <79985326+Stefania1203@users.noreply.github.com>
2021-03-04 00:52:13 +0300

    Update RemoveExtraBlanks.php

--- lw3/RemoveExtraBlanks.php
+++ lw3/RemoveExtraBlanks.php
@@ -3,7 +3,7 @@ header("Content-Type: text/plain");
 $text = $_GET['text']; /* сохранили параметр text из GET в переменной */
 $word = explode(" ", $text);
 $space = '';
-foreach ($word as $w)  /* цикл происходит перебор значений в массиве word */
+foreach ($word as $w)   /* цикл происходит перебор значений в массиве word */
 {
   echo $space.$w;
   $space = ' ';
