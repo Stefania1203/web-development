75080190f2030b4aee4ab95acf5962e2626fe99f
Stefania1203 <79985326+Stefania1203@users.noreply.github.com>
2021-03-04 11:02:13 +0300

    Add files via upload

--- lw3/RemoveExtraBlanks.php
+++ lw3/RemoveExtraBlanks.php
@@ -1,1 +1,14 @@
-
+<?php
+header("Content-Type: text/plain");
+$text = $_GET['text'];
+if ($text == null) {
+  echo "Parameter 'text' not found!";
+}  else {
+  $text = trim($text);
+  while( strpos($text,"  ") != false)
+  {
+    $text = str_replace("  ", " ", $text);
+  }
+  echo $text;
+}
+?>
