6619c7d4e59be7e266b7f9dfc158838b67775ddd
Stefania1203 <79985326+Stefania1203@users.noreply.github.com>
2021-03-04 11:05:23 +0300

    Add files via upload

---
+++ lw3/Identifier.php
@@ -0,0 +1,31 @@
+<?php
+  header("Content-Type: text/plain");
+  $identifier = $_GET['identifier'];
+  if (ctype_alpha($identifier[0])) /* если первая буква */ 
+  {
+    /* с помощью цикла ищем первую не букву */
+    for ($i = 0; $i < strlen($identifier); $i++){
+      if (!ctype_alpha($identifier[$i])) {
+        break;
+      }
+    }
+    /* с помощью цикла ищем последнюю букву */
+    for ($j = strlen($identifier) - 1; $j > 0; $j--){
+      if (ctype_alpha($identifier[$j])) {
+        break;
+      }
+    }
+    if ( $i > $j) /* если первая "не буква" стоит дальше последней буквы */
+    {
+      echo 'yes';
+    }
+    else
+    {
+      echo 'no';
+    }
+  }
+  else
+  {
+    echo 'no';
+  }
+?>
