d83c9215367560ec0575f4a840476165934a74fa
Stefania1203 <79985326+Stefania1203@users.noreply.github.com>
2021-03-04 21:55:49 +0300

    Update RemoveExtraBlanks.php

--- lw3/RemoveExtraBlanks.php
+++ lw3/RemoveExtraBlanks.php
@@ -1,14 +1,21 @@
 <?php
 header("Content-Type: text/plain");
-$text = $_GET['text'];
-if ($text === null) {
-  echo "Parameter 'text' not found!";
-}  else {
-  $text = trim($text);     /* удаление пробелов перед и после строки */
-  while( strpos($text,"  ") != false) /* 2 значения не равны входим в цикл */
-  {
-    $text = str_replace("  ", " ", $text);
-  }
-  echo $text;
+function getGETParametr(string $valueParameter): ?string
+{
+    return isset($_GET[valueParameter]) ? (string) $_GET[$valueParameter] : null;
+}	  
+$text = getGETParametr("text");
+if ($text === null) 
+{
+    echo "Parameter 'text' not found!";
+}  
+else 
+{
+    $text = trim($text);     /* удаление пробелов перед и после строки */
+    while( strpos($text,"  ") != false) /* 2 значения не равны входим в цикл */
+    {
+        $text = str_replace("  ", " ", $text);
+    }
+    echo $text;
 }
 ?>
