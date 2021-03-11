2e15b19189624fb43286034a569baa53b8a5298b
Stefania1203 <79985326+Stefania1203@users.noreply.github.com>
2021-03-11 18:23:36 +0300

    Update Identifier.php

--- lw3/Identifier.php
+++ lw3/Identifier.php
@@ -5,37 +5,41 @@ function getGETParameter(string $valueParameter): ?string
     return isset($_GET[$valueParameter]) ? (string) $_GET[$valueParameter] : null;
 }	  
 $identifier = getGETParameter("identifier");
-if (ctype_alpha($identifier[0])) /* если первая буква */ 
+if ($identifier === null)
 {
-     /* с помощью цикла ищем первую не букву */
-     for ($i = 0; $i < strlen($identifier); $i++)
-     {
-         if (!ctype_alpha($identifier[$i])) 
-         {
-             break;
-         }
-     }
-     /* с помощью цикла ищем последнюю букву */
-     for ($j = strlen($identifier) - 1; $j > 0; $j--)
-     {
-         if (ctype_alpha($identifier[$j]))  
-         {
-             break;
-         }
-     }
-     if ( $i > $j) /* если первая "не буква" стоит дальше последней буквы */
-     {
-          echo 'yes';
-     }
-     else
-     {
-         echo 'no';
-     }
-}
+	echo "Параметр не найден";
+} 
 else
 {
-    echo 'no';
-}
-{
-    echo 'no';
+    if (ctype_alpha($identifier[0])) /* если первая буква */ 
+    {
+         /* с помощью цикла ищем первую не букву */
+         for ($i = 0; $i < strlen($identifier); $i++)
+        {
+             if (!ctype_alpha($identifier[$i])) 
+             {
+                 break;
+             }
+         }
+     /* с помощью цикла ищем последнюю букву */
+         for ($j = strlen($identifier) - 1; $j > 0; $j--)
+         {
+             if (ctype_alpha($identifier[$j]))  
+             {
+                 break;
+             }
+         }
+         if ( $i > $j) /* если первая "не буква" стоит дальше последней буквы */
+         {
+              echo 'yes';
+         }
+         else
+         {
+              echo 'no';
+         }
+    }
+    else
+    {
+        echo 'no';
+    }
 }
