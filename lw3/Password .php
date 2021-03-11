24de1c21dbd43e02baafc23bd33acc1f243478ea
Stefania1203 <79985326+Stefania1203@users.noreply.github.com>
2021-03-11 21:24:02 +0300

    Update Password .php

--- lw3/Password .php
+++ lw3/Password .php
@@ -7,13 +7,14 @@ function getGETParameter(string $valueParameter): ?string
 $pw = getGETParameter("password");
 if ($pw === null)
 {
-    echo "Параметр не найден";
+   echo "Параметр не найден";
 } 
 else
 {
     /* начинаем с длинны */
     $n = strlen($pw);
     $safety = 4 * $n;
+    echo "Длина: $safety \n";
     /* считаем цифры */
     $n = 0;
     for ($i = 0; $i < strlen($pw); $i++)
@@ -21,14 +22,15 @@ else
         if (ctype_digit($pw[$i])) $n++;
     }
     $safety += $n * 4;
+    echo "С учетом цифр: $safety \n";
     /* считаем символы в верхнем регистре */
     $n = 0;
     for ($i = 0; $i < strlen($pw); $i++)
     {
-        if (ctype_upper($pw[$i])) $n++;
+    if (ctype_upper($pw[$i])) $n++;
     }
     $safety += (strlen($pw) - $n) * 2;
-
+    echo "C учетом символов в верхнем регистре: $safety \n";
     /* считаем символы в нижнем регистре */
     $n = 0;
     for ($i = 0; $i < strlen($pw); $i++)
@@ -36,16 +38,19 @@ else
         if (ctype_lower($pw[$i])) $n++;
     }
     $safety += (strlen($pw) - $n) * 2;
+    echo "C учетом символов в нижнем регистре: $safety \n";
     /* снова считаем цифры */
     $n = 0;
     for ($i = 0; $i < strlen($pw); $i++)
     {
-        if (ctype_digit($pw[$i])) $n++;
+       if (ctype_digit($pw[$i])) $n++;
     }
     if ($n == strlen($pw)) /* если пароль состоит только из цифр */
     {
-        $safety -= strlen($pw);
+       $safety -= strlen($pw);
+       echo "C учетом того если пароль состоит только из цифр: $safety \n";
     }
+
     /* считаем буквы */
     $n = 0;
     for ($i = 0; $i < strlen($pw); $i++)
@@ -55,13 +60,15 @@ else
     if ($n == strlen($pw)) /* если пароль состоит только из букв */
     {
         $safety -= strlen($pw);
+        echo "C учетом того если пароль состоит только из букв: $safety \n";
     }
-    /* повторяющиеся символы */
-    for ($i = 0; $i < strlen($pw); $i++)
-    {
-        $n = substr_count($pw, $pw[$i]);
-        if ($n > 1) $safety -= $n;
-    }
-    echo "Надежность пароля $safety";  
+
+  /* повторяющиеся символы */
+  for ($i = 0; $i < strlen($pw); $i++)
+  {
+      $n = substr_count($pw, $pw[$i]);
+      if ($n > 1) 
+      $safety -= $n;
   }
-?>
+  echo "Надежность пароля: $safety";  
+  }
